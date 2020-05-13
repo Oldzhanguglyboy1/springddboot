32022805dbcf1d27f84e6fd9dd1c2235ad937cbd
张杨 <zy612830@163.com>
2020-05-13 10:50:10 +0800

    dev2.0内容提交

--- src/main/java/com/offcn/springbootdemo/SpringbootdemoApplication.java
+++ src/main/java/com/offcn/springbootdemo/SpringbootdemoApplication.java
@@ -15,7 +15,6 @@ public class SpringbootdemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootdemoApplication.class, args);
-        System.out.println("我是1.0版");
-        System.out.println("我是2.0");
+        System.out.println("我是正式版");
     }
 }
